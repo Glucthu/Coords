086ccf70bef3bceb7eea1d3e6ca6ceed1731ef43
Glucthu <53957965+Glucthu@users.noreply.github.com>
2022-02-13 15:15:00 -0300

    added movement

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
-    var coords = "X: " + x + ", Y: " + y;
-    document.getElementById("coords").innerHTML = coords;
-  }
+function teleportCircle(){
+  let x = Math.floor(Math.random() * window.innerWidth);
+  let y = Math.floor(Math.random() * window.innerHeight);
+
+  document.getElementById("circle").style.left = x;
+  document.getElementById("circle").style.top = y;
+}
+
+function showCoords(event){
+  let x = event.clientX;
+  let y = event.clientY;
+  let coords = "X: " + x + ", Y: " + y;
+  document.getElementById("coords").innerHTML = coords;
+}
